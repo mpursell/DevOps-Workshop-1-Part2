ef66f0c5803b264464d03e68f933fd6286ec0a5f
Mike <michael.pursell@gmail.com>
2021-11-11 20:05:27 +0000

    Added route and submit template, but getting Method Not Allowed error on /films/submit URL

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
+from flask import request 
+from werkzeug.utils import redirect
 import csv
 
 app = Flask(__name__)
+
+def append_CSV(film, rating):
+    with open('films.csv', 'a') as csv:
+
+        string = film + ',' + rating
+        csv.write(string + '\n')
+
+    csv.close()
+
+
 
 @app.route('/hello')
 def hello_world():
@@ -17,7 +29,7 @@ def get_Films():
         
         for row in reader:
             filmsList.append({'title': row[0], 'rating':row[1]})
-
+    filmsCsv.close()
     return render_template('films.html', filmsList=filmsList) 
 
 @app.route('/films/table')
@@ -32,6 +44,16 @@ def films_filter():
             if row[1] == stars:
                 filmsList.append({'title': row[0], 'rating':row[1]})
             
-
+    filmsCsv.close()
     return render_template('films.html', filmsList=filmsList)
+
+@app.route('/films/submit', methods = ['POST'])
+def submit_film():
+
+    filmName = request.form.get('Film')
+    rating = request.form.get('Stars')
+
+    append_CSV(filmName, rating)
+
+    return render_template('/films/submit')
     
